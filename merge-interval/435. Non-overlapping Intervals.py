e4aee143a69b4c5b87788e279fe2af7d63267a8c
zlin888 <zhitaolin@outlook.com>
2020-07-11 22:33:20 +0800

    dev: 435 method 2

--- merge-interval/435. Non-overlapping Intervals.py
+++ merge-interval/435. Non-overlapping Intervals.py
@@ -18,3 +18,18 @@ class Solution:
         return output
 
 # 贪心算法：找出尽量多不重合的interval （经典的办公室会议问题，排课问题），按照结束时间排序
+class Solution:
+    def eraseOverlapIntervals(self, intervals):
+        if not intervals:
+            return 0
+        intervals.sort(key=lambda _:_[1])
+        prev = None
+        count = 1
+        for interval in intervals:
+            if not prev:
+                prev = interval
+            else:
+                if not prev[1] > interval[0]:
+                    count += 1
+                    prev = interval
+        return len(intervals) - count
